b0159c8b0a7337650c7eebe48436b76aff589f02
GideonPolas <biurokles@gmail.com>
2025-11-10 19:02:44 +0100

    Opening file

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron/main')
+const path = require('node:path')
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
+async function handleFileOpen () {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (!canceled) {
+    return filePaths[0]
+  }
+}
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
-
-  win.loadFile('index.html')
+  mainWindow.loadFile('main/index.html')
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('dialog:openFile', handleFileOpen)
   createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
